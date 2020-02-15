cb7dcd620ae36baa6ac97a1c029ad65318d43c8e
Raynos <raynos2@gmail.com>
2020-02-15 20:57:43 +0100

    update build meta data

--- build/test/test-harness.js
+++ build/test/test-harness.js
@@ -1,7 +1,7 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
-const AWS = require("aws-sdk");
-const tape = require("tape");
+const AWS = require("@pre-bundled/aws-sdk");
+const tape = require("@pre-bundled/tape");
 const tapeCluster = require("tape-cluster");
 const index_1 = require("../src/index");
 class TestHarness {
